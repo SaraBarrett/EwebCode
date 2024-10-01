2e0fa2aa9721c74c06de3bd2e1b095414da7c234
SaraBarrett <sara.c.mon@gmail.com>
2024-10-01 12:08:58 +0100

    args

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,21 @@
 
 
-function alertHello(){
-    alert('hello world!');
+// function alertHello(){
+//     alert('hello world!');
+// }
+
+// function printHeart(){
+//     alert('<3');
+// }
+
+function printAlert(string){
+
+    alert('olá a mensagem que tenho para ti é '+string);
 }
 
-function printHeart(){
-    alert('<3');
+function rant(message){
+    for(let rant=1; rant<4; rant++){
+        alert(rant+': '+message.toUpperCase());
+    }
+
 }
