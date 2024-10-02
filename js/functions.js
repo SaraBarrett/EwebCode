ba2a83a147450ca6661b64d4b3fb059dc05dcce2
SaraBarrett <sara.c.mon@gmail.com>
2024-10-02 08:59:37 +0100

    funções c/args1

--- js/functions.js
+++ js/functions.js
@@ -13,9 +13,41 @@ function printAlert(string){
     alert('olá a mensagem que tenho para ti é '+string);
 }
 
+function tellMeWhatYHate(){
+
+    let hate = prompt('diz-me o que odeias!');
+    rant(hate);
+}
+
 function rant(message){
     for(let rant=1; rant<4; rant++){
         alert(rant+': '+message.toUpperCase());
     }
+}
+
+let media = calc(18, 18, '+')/2;
+
+function calc(num1, num2, op){
+    switch(op){
+        case '+':
+        alert('o resultado é ' + (num1+num2));
+        break;
+
+        case '*':
+        alert('o resultado é ' + (num1*num2));
+        break;
+
+        case '-':
+            alert('o resultado é ' + (num1-num2));
+            break;
+
+        case '/':
+            alert('o resultado é ' + (num1/num2));
+            break;
+
+    }
+
+
+   
 
 }
